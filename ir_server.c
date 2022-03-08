ebf881f1b559d37182b5165ba1fb22670b15f9cf
Johan Nilsson <h.j.i.nilsson@gmail.com>
2022-03-08 09:38:25 +0100

    Add some basic lirc code to verify functionality
    Proof of concept using lirc library. Getting socket and send one command
    to receiver.

--- ir_server.c
+++ ir_server.c
@@ -6,6 +6,7 @@
 /* -- Includes ------------------------------------------------------------------------------------------------------ */
 
 #include <stdio.h>
+#include "lirc_client.h"
 
 /* -- Private macros ------------------------------------------------------------------------------------------------ */
 
@@ -18,12 +19,23 @@
 /* -- Public functions ---------------------------------------------------------------------------------------------- */
 
 /**
- * @brief  Function description
- * @param  Parameter description
- * @retval void
+ * @brief  IR server entry point and main loop
+ * @param  void
+ * @retval int, functional code returned on exit. Will return 0 if OK.
  */
 int main(void) {
     printf("Starting IR server...\n");
+    const int fd = lirc_get_local_socket(NULL, 0);
+    printf("IR socket: %d\n", fd);
+
+    if (fd < 0) {
+        printf("Process error: Could not get local socket\n");
+        return -1;
+    }
+
+    if (lirc_send_one(fd, "RC-830", "KEY_POWER") == -1) {
+        printf("Process error: Unable to send IR command\n");
+    }
 
     return 0;
 }
